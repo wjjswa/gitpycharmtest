be48215b84e3c106e5dad1c3b4e1764b9e6f9c2f
wjjswa <76581390+wjjswa@users.noreply.github.com>
2024-03-28 10:58:45 +0000

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,11 +4,10 @@
 #SBATCH --nodes=1
 
 # set max wall-clock time
-#SBATCH --time=10:00:00
+#SBATCH --time=00:01:00
 
 # set name of job
-#SBATCH --job-name=job123
-
+#SBATCH --job-name=test1
 # set number of GPUs
 #SBATCH --gres=gpu:1
 
@@ -16,10 +15,9 @@
 #SBATCH --mail-type=ALL
 
 # send mail to this address
-#SBATCH --mail-user=john.brown@gmail.com
+#SBATCH --mail-user=jw840@sussex.ac.uk
 
 # load the module for python or anaconda if needed
-# module load python/3.8.5 or something similar
-
+# module load python/3.8.5 
 # run the application
 python test.py
